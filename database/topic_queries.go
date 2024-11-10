a05cb1310c3ebd332b067787c934a401f7320cdb
dDogge <douglasfjallrud@gmail.com>
2024-11-10 12:47:30 +0100

    AddTopic database logic implemented

--- database/topic_queries.go
+++ database/topic_queries.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 )
 
@@ -18,4 +20,37 @@ func CreateTopicTable(db *sql.DB) {
 	if err != nil {
 		log.Fatal("Error creating topic table: ", err)
 	}
+}
+
+func AddTopic(db *sql.DB, title, username string) error {
+	var creator_id int
+	err := db.QueryRow("SELECT id FROM users WHERE username = ?", username).Scan(&creator_id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return errors.New("User not found")
+		} else {
+			log.Fatal(err)
+		}
+		log.Printf("Error fetching creator_id: %v", err)
+		return fmt.Errorf("Could not fetch creator_id: %w", err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO topics (title, creator_id) VALUES (?, ?)")
+	if err != nil {
+		log.Printf("Error preparing statement: %v", err)
+		return fmt.Errorf("Could not prepare statement: %w", err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(title, creator_id)
+	if err != nil {
+		if isUniqueConstraintError(err) {
+			return errors.New("Topic tilte already exists")
+		}
+		log.Printf("Error executing statement: %v", err)
+		return fmt.Errorf("Could not execute statement: %w", err)
+	}
+
+	log.Println("Topic added successfully:", title)
+	return nil
 }
