1deb260800a18f2ffdb7e5e68a9f915d412d7386
dDogge <douglasfjallrud@gmail.com>
2024-12-09 23:33:01 +0100

    ChangeEmail handler added

--- handlers/messages.go
+++ handlers/messages.go
@@ -30,6 +30,16 @@ type ChangePasswordResponse struct {
 	Error   string `json:"error,omitempty"`
 }
 
+type ChangeEmailRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type ChangeEmailResponse struct {
+	Message string `json:"message"`
+	Error   string `json:"error,omitempty"`
+}
+
 func CreateUserHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	var reqBody struct {
 		Username string `json:"username"`
@@ -134,6 +144,46 @@ func ChangePasswordHandler(db *sql.DB) http.HandlerFunc {
 
 		resp := ChangePasswordResponse{
 			Message: "password changed successfully",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
+func ChangeEmailHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var reqBody ChangeEmailRequest
+		err := json.NewDecoder(r.Body).Decode(&reqBody)
+		if err != nil {
+			http.Error(w, "invalid JSON format", http.StatusBadRequest)
+			return
+		}
+
+		if reqBody.Username == "" || reqBody.Email == "" {
+			http.Error(w, "all fields (username, current_password, new_password) are required", http.StatusBadRequest)
+			return
+		}
+
+		err = database.ChangeEmail(db, reqBody.Username, reqBody.Email)
+		if err != nil {
+			var statusCode int
+			if errors.Is(err, sql.ErrNoRows) || err.Error() == "incorrect current password" {
+				statusCode = http.StatusUnauthorized
+			} else {
+				statusCode = http.StatusInternalServerError
+			}
+			http.Error(w, err.Error(), statusCode)
+			return
+		}
+
+		resp := ChangeEmailResponse{
+			Message: "email changed successfully",
 		}
 
 		w.Header().Set("Content-Type", "application/json")
