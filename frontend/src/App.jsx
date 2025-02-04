a20662488bb8c4056d864aa48417ba42eb6b4ddd
dDogge <douglasfjallrud@gmail.com>
2025-02-04 16:57:58 +0100

    Added logo and played around a little in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
+import logo from './images/logo.png';
 
 function App() {
   return (
     <div className="container">
       <div className="bar">
-      <h1 className="logotext">Brainwave</h1>
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
       </div>
       <p>TODO</p>
     </div>
   );
 }
 
-export default App
+export default App;
