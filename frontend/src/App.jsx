f1bf45e64431c31e64d5a3e9f4f7388cf89f630e
dDogge <douglasfjallrud@gmail.com>
2025-02-07 17:15:04 +0100

    Added router and other pages to be implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import logo from './images/logo.png';
 
-function App() {
+function HomePage() {
   return (
     <div className="container">
       <div className="bar">
         <h1 className="logotext">Brainwave</h1>
         <img className="logo" src={logo} alt="Logo" />
-        <button className="loginButton">login</button>
+        <Link to="/login">
+          <button className="loginButton">login</button>
+        </Link>
       </div>
       <div className="subcontainer">
         <form className="searchBar"> 
@@ -22,6 +25,105 @@ function App() {
         </div>
       </div>
     </div>
+  );
+}
+
+function LoginPage() {
+  return (
+    <div className="container">
+      <div className="bar">
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="subcontainer">
+      </div>
+    </div>
+  );
+}
+
+function ForgotPasswordPage() {
+  return (
+    <div className="container">
+      <div className="bar">
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="subcontainer">
+      </div>
+    </div>
+  );
+}
+
+function UserSettingPage() {
+  return (
+    <div className="container">
+      <div className="bar">
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="subcontainer">
+      </div>
+    </div>
+  );
+}
+
+function UserFrontPage() {
+  return (
+    <div className="container">
+      <div className="bar">
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="subcontainer">
+        <form className="searchBar"> 
+         <input type="search" id="query" name="q" placeholder="Search..."></input>
+         <button className="searchButton">Search</button>
+        </form>
+        <button className="recentButton">Recent</button>
+        <button className="likesButton">Likes</button>
+        <button className="oldestButton">Oldest</button>
+        <div className="topicsContainer">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TopicPage() {
+  return (
+    <div className="container">
+      <div className="bar">
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="subcontainer">
+      </div>
+    </div>
+  );
+}
+
+function TopicPageUser() {
+  return (
+    <div className="container">
+      <div className="bar">
+        <h1 className="logotext">Brainwave</h1>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="subcontainer">
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
